d514d3e3f8abd7e87b8517f4f550958112ff4a19
Ishani610 <ishanisaxena04@gmail.com>
2026-02-23 14:48:59 +0530

    Updated palindrome logic

--- src/Palindrome/PalindromeCheckerApp.java
+++ src/Palindrome/PalindromeCheckerApp.java
@@ -1,7 +1,25 @@
 package Palindrome;
-
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     static void main(String[] args) {
-        System.out.println();
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter a word: ");
+            String input = sc.nextLine();
+
+            String reversed = "";
+
+            for (int i = input.length() - 1; i >= 0; i--) {
+                reversed = reversed + input.charAt(i);
+            }
+
+            if (input.equalsIgnoreCase(reversed)) {
+                System.out.println("It is a Palindrome ✅");
+            } else {
+                System.out.println("It is NOT a Palindrome ❌");
+            }
+
+            sc.close();
+        }
     }
-}
