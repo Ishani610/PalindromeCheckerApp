b9178fbb09cca8a9857f658c694befb42ec7e14a
Ishani610 <ishanisaxena04@gmail.com>
2026-03-10 15:54:33 +0530

    Fix UC10 palindrome code

--- src/Palindrome/PalindromeCheckerApp.java
+++ src/Palindrome/PalindromeCheckerApp.java
@@ -4,6 +4,24 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
+    public static boolean isPalindrome(String input) {
+
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -11,26 +29,7 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        // Normalize string (remove spaces and convert to lowercase)
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
-
-        int start = 0;
-        int end = normalized.length() - 1;
-
-        boolean isPalindrome = true;
-
-        while (start < end) {
-
-            if (normalized.charAt(start) != normalized.charAt(end)) {
-                isPalindrome = false;
-                break;
-            }
-
-            start++;
-            end--;
-        }
-
-        if (isPalindrome)
+        if (isPalindrome(input))
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
