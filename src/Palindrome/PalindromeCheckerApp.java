451d1e326a7206a348dfa76de65b071e00c4bada
Ishani610 <ishanisaxena04@gmail.com>
2026-03-10 15:21:58 +0530

    UC8: Linked list palindrome checker

--- src/Palindrome/PalindromeCheckerApp.java
+++ src/Palindrome/PalindromeCheckerApp.java
@@ -1,34 +1,80 @@
 package Palindrome;
 
-import java.util.*;
+import java.util.Scanner;
+
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
 public class PalindromeCheckerApp {
+
+    static Node createList(String str) {
+        Node head = null, temp = null;
+
+        for (char c : str.toCharArray()) {
+            Node newNode = new Node(c);
+
+            if (head == null) {
+                head = newNode;
+                temp = newNode;
+            } else {
+                temp.next = newNode;
+                temp = newNode;
+            }
+        }
+        return head;
+    }
+
+    static boolean checkPalindrome(Node head) {
+
+        Node slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node prev = null, curr = slow;
+
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node first = head, second = prev;
+
+        while (second != null) {
+            if (first.data != second.data)
+                return false;
+
+            first = first.next;
+            second = second.next;
+        }
+
+        return true;
+    }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        Deque<Character> deque = new LinkedList<>();
 
-        System.out.print("Enter word: ");
-        String word = sc.nextLine();
+        System.out.print("Enter string: ");
+        String input = sc.nextLine();
 
-        for (char c : word.toCharArray()) {
-            deque.add(c);
-        }
+        Node head = createList(input);
 
-        boolean isPalindrome = true;
-
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome)
+        if (checkPalindrome(head))
             System.out.println("Palindrome");
         else
-            System.out.println("Not palindromee");
+            System.out.println("Not Palindrome");
 
         sc.close();
     }
